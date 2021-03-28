27408c187ee2d4ee8d0590056f405b4a984ee2a3
Mary Fagan <m5fgn@outlook.com>
2021-03-28 16:37:03 +0100

    Added check_winner()

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,31 +1,14 @@
 from flask import render_template, request, redirect
 from play import app
-from models.game import *
-from models.player import *
-
-@app.route('/index')
-def index():
-    return render_template('index.html')
-
-@app.route('/instructions')
-def instructions():
-    return render_template('instructions.html', title='Instructions')
-
-@app.route('/play')
-def playcomputer():
-    return render_template('play.html', title='Player 1 vrs The Computer')
+from models.game import check_winner
+from models.player import Player
 
 
-# @app.route('/index', methods=['post'])
-# def players():
-#     player1_name = request.form['player1_name']
-#     player1_choice = request.form['player1_choice']
-#     player_1 = Player(player1_name, player1_choice)
-#     player2_name = request.form['player2_name']
-#     player2_choice = request.form['player2_choice']
-#     player_2 = Player(player2_name, player2_choice)
-#     check_winner(player_1, player_2)
-#     return redirect('/results')
+# In browser gameplay
+
+@app.route('/')
+def player1():
+    return 'Player 1 add your choice'
 
 @app.route('/rock')
 def rock_addplayer2():
@@ -39,43 +22,87 @@ def paper_addplayer2():
 def scissors_addplayer2():
     return 'Player 2 add your choice'
 
+@app.route('/<player1_choice>/<player2_choice>')
+def browser_play(player1_choice, player2_choice):
+    player1 = Player('Player 1', player1_choice)
+    player2 = Player('Player 2', player2_choice)
+    winner = check_winner(player1, player2)
+    return f'The winner is {winner}'
 
-@app.route('/rock/paper')
-def rockpaper():
-    return 'Player 2 has won'
+# FIXME Add the re-direct to the return of browser_play() 
+@app.route('/<player1_choice>/<player2_choice>/basic_results')
+def browser_play_results(player1_choice, player2_choice):
+    player1 = Player('Player 1', player1_choice)
+    player2 = Player('Player 2', player2_choice)
+    winner = check_winner(player1, player2)
+    return render_template('basic_results.html', player1=player1, player2=player2, winner=winner, title='Winners')
 
-@app.route('/rock/scissors')
-def rockscissors():
-    return 'Player 1 has won'
+# GUI Game Play
 
-@app.route('/rock/rock')
-def rockrock():
-    return 'The game is a draw'
+@app.route('/index')
+def index():
+    return render_template('index.html')
 
-@app.route('/paper/paper')
-def paperpaper():
-    return 'The game is a draw'
+@app.route('/instructions')
+def instructions():
+    return render_template('instructions.html', title='Instructions')
 
-@app.route('/paper/scissors')
-def paperscissors():    
-    return 'Player 2 has won'
+@app.route('/play')
+def playcomputer():
+    return render_template('play.html', title='Player 1 vrs The Computer')
 
-@app.route('/paper/rock')
-def paperrock():
-    return 'Player 1 has won'
-
-@app.route('/scissors/paper')
-def scissorspaper():
-    return 'Player 1 has won'
-
-@app.route('/scissors/scissors')
-def scissorsscissors():
-    return 'The game is a draw'
-
-@app.route('/scissors/rock')
-def scissorsrock():
-    return 'Player 2 has won'
+# FIXME Get the player-name and choice from the form and add the re-direct.
+@app.route('/index', methods=['post'])
+def play_game():
+    player1_name = request.form['player1_name']
+    player1_choice  = request.form['player1_choice']
+    player1 = Player(player1_name, player1_choice)
+    player2_name = request.form['player2_name']
+    player2_choice = request.form['player2_choice']
+    player2 = Player(player2_name, player2_choice)
+    winner = check_winner(player1, player2)
+    return winner
+    # return redirect('/results')
 
 @app.route('/results')
 def results():
-    return render_template('results.html', title='Winner', players=players)
+    return render_template('results.html', title='Winner')
+
+# No Longer needed not that check_winner() has been added 
+
+# @app.route('/rock/paper')
+# def rockpaper():
+#     return 'Player 2 has won'
+
+# @app.route('/rock/scissors')
+# def rockscissors():
+#     return 'Player 1 has won'
+
+# @app.route('/rock/rock')
+# def rockrock():
+#     return 'The game is a draw'
+
+# @app.route('/paper/paper')
+# def paperpaper():
+#     return 'The game is a draw'
+
+# @app.route('/paper/scissors')
+# def paperscissors():    
+#     return 'Player 2 has won'
+
+# @app.route('/paper/rock')
+# def paperrock():
+#     return 'Player 1 has won'
+
+# @app.route('/scissors/paper')
+# def scissorspaper():
+#     return 'Player 1 has won'
+
+# @app.route('/scissors/scissors')
+# def scissorsscissors():
+#     return 'The game is a draw'
+
+# @app.route('/scissors/rock')
+# def scissorsrock():
+#     return 'Player 2 has won'
+
--- models/game.py
+++ models/game.py
@@ -1,6 +1,22 @@
-class Game():
+from models.player import *
 
-    def __init__(self, player_1, player_2):
-        self.player_1 = player_1
-        self.player_2 = player_2
-
+def check_winner(player1, player2):
+    winner = ' '
+    if player1.choice == player2.choice:
+        winner = 'Draw'
+    elif player1.choice == 'rock':
+        if player2.choice == 'paper':
+            winner = 'player2'
+        elif player2.choice == 'scissors':
+            winner = 'player1'
+    elif player1.choice == 'paper':
+        if player2.choice == 'rock':
+            winner = 'player1'
+        elif player2.choice == 'scissors':
+            winner = 'player2'
+    elif player1.choice == 'scissors':
+        if player2.choice == 'rock':
+            winner = 'player2'
+        elif player2.choice == 'paper':
+            winner = 'player2'
+    return winner
